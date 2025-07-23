c72f09905dbe393a424aef46178e4f97254a0e67
Steve Struemph <steve@stevestruemph.com>
2025-07-22 21:38:46 -0500

    Remove build files for unused blocks.

--- wp-content/plugins/matchbox-blocks/build/blocks/grid-item/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '7fc61835bf4d8e38c1c1');
--- wp-content/plugins/matchbox-blocks/build/blocks/grid-item/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '9c9dc824ab192c66e6e8');
